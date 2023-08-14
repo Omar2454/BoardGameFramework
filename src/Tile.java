3a3ef064b4d27e575704bc4601c835a7a6f86e95
Ali Fadl <ali.ahmed.fadl@hotmail.com>
2023-08-14 21:58:15 +0300

    1st commit

---
+++ src/Tile.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Tile {
+ private List units;
+
+    public Tile() {
+        this.units = new LinkedList();
+    }
+
+    protected void addUnit(Unit unit) {
+        units.add(unit);
+    }
+
+    protected void removeUnit(Unit unit) {
+        units.remove(unit);
+    }
+
+    public void removeUnits() {
+    }
+
+    public List getUnits() {
+        return units;
+    }
+}
